92bc95dbf8e3ad357911f115cc95b6df6816b9a5
cmkernan <kernanc@bc.edu>
2016-04-11 22:41:35 -0400

    Update login.php

--- login.php
+++ login.php
@@ -65,7 +65,7 @@
                   <input type = "submit" value = " Submit "/><br />
                </form>
                <p>Don't have an account? <a href="http://cscilab.bc.edu/~concham/Project/ProjectForm.html"> Register Here </a> </p>
-               <p>Forgot your password?> <a href="">Get New Password</a></p>
+               <p>Forgot your password? <a href="">Get New Password</a></p>
                
                <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
 					
