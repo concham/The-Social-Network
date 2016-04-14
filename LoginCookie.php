a4929ca019c6cb749e63d75cf8fb156a7598a852
Catherine Kernan <Catherine@catherinekernan.bc.edu>
2016-04-14 16:52:49 -0400

    LoginCookie.php

---
+++ LoginCookie.php
@@ -0,0 +1,40 @@
+<?php
+$debug = 1;
+
+if ( ! isset( $_POST['username'] ) or  
+	 ! isset( $_POST['password'] ) or 
+	( 0 == checklogin( $_POST['username'], $_POST['password'] ) ) ) {
+  header("Location: loginCookieForm.php");
+ } else { 
+	
+	// Store the login information in cookies	
+	setcookie('loginCookieUser', $_POST['username']);
+  	header("Location: showcustomer.php");
+}
+// checklogin sees if an entry exists with the name password pair passed.
+// returns true if so, false otherwise.
+function checklogin($username, $password){
+	$dbc = connectToDB("kernanc");
+	$encodepassword = sha1($password);
+	$result = performQuery($dbc, 
+		"select * FROM community where username='$username' and password='$encodepassword'");
+	$matches = mysqli_num_rows($result);
+	mysqli_free_result($result);
+	disconnectFromDB($dbc);
+	return($matches == 1);
+}
+// Modified connectToDB takes database as an argument, returns database connection
+function connectToDB($community){
+	$dbc= @mysqli_connect("http://cscilab.bc.edu/phpmyadmin", "kernanc", "Y5NevHgP", $community) or
+					die("Connect failed: ". mysqli_connect_error());
+	return ($dbc);
+}
+function disconnectFromDB($dbc){
+	mysqli_close($dbc);
+}
+// Modified PeformQuery, takes the database and query as arguments, returns result set
+function performQuery($dbc, $query){
+	//echo "My query is >$query< <br />";	
+	$result = mysqli_query($dbc, $query) or die("bad query".mysqli_error($dbc));
+	return ($result);
+}?>
