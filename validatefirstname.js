26a7426475b53418e529471ab3592b081ebcaa74
barchr <barthchrisp@gmail.com>
2016-04-14 15:51:07 -0400

    typo correction

--- validatefirstname.js
+++ validatefirstname.js
@@ -2,11 +2,11 @@ function validateFirstName(){
 			var firstname= document.getElementById("FirstName").value ;
 			
 			if (firstname.length < 1) {
-				var errorrfirstname=document.getElementById("firstnameerror");
+				var errorfirstname=document.getElementById("firstnameerror");
 				errorfirstname.innerHTML = "Please enter a first name";
 				return false;
 			} 
-			var erroremail=document.getElementById("firstnameerror");
+			var errorfirstname=document.getElementById("firstnameerror");
 			errorfirstname.innerHTML = "";
 	
 			return true;
