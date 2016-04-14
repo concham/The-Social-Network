7c8e35484593ac432353e8866843c9b30cdec611
barchr <barthchrisp@gmail.com>
2016-04-14 16:41:20 -0400

    function confirmation added

--- validatepassword.js
+++ validatepassword.js
@@ -11,3 +11,24 @@ function validatepassword(){
 	
 			return true;
 			}
+			
+			function validateconfirmation(){
+			var confirmation= document.getElementById("confirmation").value ;
+			
+			if (confirmation.length < 1) {
+				var errorconfirmation=document.getElementById("confirmationerror");
+				errorconfirmation.innerHTML = "Please enter your password confirmation";
+				return false;
+			} 
+			
+			if(confirmation !== password) {
+			errorconfirmation= document.getElementByID("confirmationerror");
+			errorconfirmation.innerHTML= "Password must match with confirmed password";
+			return false;
+			}
+			
+			var errorconfirmation=document.getElementById("confirmationerror");
+			errorconfirmation.innerHTML = "";
+	
+			return true;
+			}
