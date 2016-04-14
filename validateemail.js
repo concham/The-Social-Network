d2948d66f8ddcdb141f767d1b6b8cc64cf088243
barchr <barthchrisp@gmail.com>
2016-04-14 15:52:51 -0400

    typo correction

--- validateemail.js
+++ validateemail.js
@@ -2,7 +2,7 @@ function validateEmail(){
 			var email= document.getElementById("email").value ;
 			
 			if (email.length < 1) {
-				var errorremail=document.getElementById("emailerror");
+				var erroremail=document.getElementById("emailerror");
 				erroremail.innerHTML = "Please enter an Email";
 				return false;
 			} 
