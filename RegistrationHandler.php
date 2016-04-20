19847839c2387e3c964a9ab75d88459e6128080c
cmkernan <kernanc@bc.edu>
2016-04-20 14:47:50 -0400

    Update RegistrationHandler.php

--- RegistrationHandler.php
+++ RegistrationHandler.php
@@ -1,11 +1,7 @@
 <?php
+	
+	include("dbconn.php");
 
-function connect_to_db($connect){
-	$connect = @mysqli_connect("localhost", "kernanc", "Y5NevHgP", "kernanc")
-		or die("Connect failed: ". mysqli_connect_error());
-}
-
-function perform_query($connect, $query){
 	$firstname = isset($_POST["firstname"]) ? $_POST["firstname"] : "";
 	$lastname = isset($_POST["lastname"]) ? $_POST["lastname"] : "";
 	$email = isset($_POST["email"]) ? $_POST["email"] : "";
@@ -14,18 +10,13 @@ function perform_query($connect, $query){
 	$school = isset($_POST["school"]) ? $_POST["school"] : "";
 	
 	$query = "INSERT INTO community(firstname, lastname, email, password, school)
-		VALUES('$firstname','$lastname','$email','$encryptpassword','$school');"
+		VALUES('$firstname','$lastname','$email','$encryptpassword','$school');";
 	
-	$result = mysqli_query($connect, $query);
-	if (mysqli_query($connect, $query)) {
-		echo "You have successfully joined The Social Network";
+	$dbc = connect_to_db("kernanc");
+	if (true == perform_query( $dbc, $query ) ) {
+			$returnstatus = array( 'status'=> "success", 'data'=> "You have successfully joined The Social Network");
 	} else {
-		 echo "Error: " . $sql . "<br>" . mysqli_error($connect);
+			$returnstatus = array( 'status'=> "failure", 'data'=> "Error: " . $sql . "<br>" . mysqli_error($connect));
 	}
-	
-$row = mysqli__fetch_array($result);
-$community_data = array('communityInfo' => $row);
-echo json_encode($community_data);
-mysqli_close($connect);
-
-?>
+		
+	echo json_encode( $returnstatus );
