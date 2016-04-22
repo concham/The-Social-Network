f3629c44af7297b859e65933322e2a4ca6129b7b
cmkernan <kernanc@bc.edu>
2016-04-22 10:58:13 -0400

    Update NewLogin.php

--- NewLogin.php
+++ NewLogin.php
@@ -1,4 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title> The Social Network: Login </title>
+	<link rel="stylesheet" type="text/css" href="CSSNewLogin.css">
+</head>
+
+<body>
+<h1 align=center> Please Log In </h1>
+
+<?php 
+	$email = isset( $_POST['email'] ) ? $_POST['email'] : "";
+	$password = isset( $_POST['password'] ) ? $_POST['password'] : "";
+	displayLoginForm( $email, $password );
+?>
+
+</body>
+</html>
+
 <?php
+function displayLoginForm($email, $password)
+{?>
+	<fieldset>
+	<form action="loginCookie.php" method="post">
+		Email:<input type="text" name="email" type="text" size="20" value="<?php echo $email; ?>"/>
+		Password:<input name="password" type="password" size="20" value="<?php echo $password; ?>"/><br />
+		<input name="submitted" type="hidden" size="20" value="true" /><br>
+		<input type="submit" name="Login" value="Login" />
+		
+	<p>Don't have an account? <a href="http://cscilab.bc.edu/~kernanc/project/TheSocialNetwork/PreRegistration.html"> Register Here! </a> </p>
+	<p>Forgot Your Password? <a href="http://cscilab.bc.edu/~concham/Project/ProjectPasswordReset.html"> Click Here! </a> </p>
+	</form>
+	</fieldset>
+<?php }
+
+/* <?php
    include("dbconn.php");
    session_start();
    
@@ -28,11 +64,7 @@
    }
 ?>
 
-<html>
-   
-   <head>
-      <title>The Social Network: Login</title>
-         <link rel="stylesheet" type="text/css" href="CSSNewLogin.css">
+        
    </head>
 		<form action="" method= "post" class="form-horizontal">
 		  <fieldset>
@@ -67,7 +99,7 @@
 		</form>
 		
 </body>
-</html>
+</html>//
 
 
 
