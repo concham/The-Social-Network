a3c8240e1701993e716f4ddce23f3bf9574c494c
cmkernan <kernanc@bc.edu>
2016-04-26 16:09:23 -0400

    Update RegistrationHandler.php

--- RegistrationHandler.php
+++ RegistrationHandler.php
@@ -2,6 +2,7 @@
 	
 	include("dbconn.php");
 
+	$membertype = isset($_POST["membertype"]) ? $_POST["membertype"] : "";
 	$firstname = isset($_POST["firstname"]) ? $_POST["firstname"] : "";
 	$lastname = isset($_POST["lastname"]) ? $_POST["lastname"] : "";
 	$email = isset($_POST["email"]) ? $_POST["email"] : "";
@@ -9,8 +10,8 @@
 	$encryptpassword = sha1($password);
 	$school = isset($_POST["school"]) ? $_POST["school"] : "";
 	
-	$query = "INSERT INTO community(firstname, lastname, email, password, school)
-		VALUES('$firstname','$lastname','$email','$encryptpassword','$school');";
+	$query = "INSERT INTO Member(membertype, firstname, lastname, email, password, school)
+		VALUES('$membertype','$firstname','$lastname','$email','$encryptpassword','$school');";
 	
 	$dbc = connect_to_db("kernanc");
 	if (true == perform_query( $dbc, $query ) ) {
