db436c6ee2637492824475c98e74fa0196736449
barchr <barthchrisp@gmail.com>
2016-04-30 16:00:13 -0400

    get to post change

--- emaillistpageall.php
+++ emaillistpageall.php
@@ -44,18 +44,20 @@
 		
 		
 		<?php
+if (isset($_GET['myBtn'])) {
+
 function sendemailtoclass(){
 include("dbconn.php");
 connect_to_db();
 $school = array(); //sets the schools as an array
-$school[] = (isset($_GET['a&s']) && $_GET['a&s'] == 1) ? 'school_name = Morrissey College of Arts & Sciences ' : '';
-$school[] = (isset($_GET['law']) && $_GET['law'] == 1) ? 'school_name = Law School' : '';
-$school[] = (isset($_GET['woods']) && $_GET['woods'] == 1) ? 'school_name = Woods College of Advancing Studies ' : '';
-$school[] = (isset($_GET['social']) && $_GET['social'] == 1) ? 'school_name = School of Social Work ' : '';
-$school[] = (isset($_GET['csom']) && $_GET['csom'] == 1) ? 'school_name = Carroll School of Management ' : '';
-$school[] = (isset($_GET['nursing']) && $_GET['nursing'] == 1) ? 'school_name = Connell School of Nursing ' : '';
-$school[] = (isset($_GET['lynch']) && $_GET['lynch'] == 1) ? 'school_name = Lynch School of Education ' : '';
-$school[] = (isset($_GET['theology']) && $_GET['theology'] == 1) ? 'school_name = School of Theology & Ministry ' : '';
+$school[] = (isset($_POST['a&s']) && $_POST['a&s'] == 1) ? 'school_name = Morrissey College of Arts & Sciences ' : '';
+$school[] = (isset($_POST['law']) && $_POST['law'] == 1) ? 'school_name = Law School' : '';
+$school[] = (isset($_POST['woods']) && $_POST['woods'] == 1) ? 'school_name = Woods College of Advancing Studies ' : '';
+$school[] = (isset($_POST['social']) && $_POST['social'] == 1) ? 'school_name = School of Social Work ' : '';
+$school[] = (isset($_POST['csom']) && $_POST['csom'] == 1) ? 'school_name = Carroll School of Management ' : '';
+$school[] = (isset($_POST['nursing']) && $_POST['nursing'] == 1) ? 'school_name = Connell School of Nursing ' : '';
+$school[] = (isset($_POST['lynch']) && $_POST['lynch'] == 1) ? 'school_name = Lynch School of Education ' : '';
+$school[] = (isset($_POST['theology']) && $_POST['theology'] == 1) ? 'school_name = School of Theology & Ministry ' : '';
 $temparray = array_filter($school); //filters the array for empty values
 if (!empty($temparray)) {
    $selection = 'WHERE '.join('OR',$temparray);   // assigns a WHERE-clause with the non-empty values in the $temparray
@@ -72,11 +74,12 @@ while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
 	$emaillist+= "$email, ";
 	}
 
-	$to= $emaillist
+	$to= $emaillist;
 	$subject= isset($_GET["subject"]) ? $_GET["subject"] : "";
 	$body=isset($_GET["message"]) ? $_GET["message"] : "";
 	$headers = 'From: The-Social-Network@bc.edu';
 	mail( $to, $subject, $body, $headers );
+	}
 	}
 ?>
 
