ff44d5475e86de9516e81587a4852020018666f2
cmkernan <kernanc@bc.edu>
2016-05-01 17:27:41 -0400

    Update PasswordResetHandler.php

--- PasswordResetHandler.php
+++ PasswordResetHandler.php
@@ -12,24 +12,13 @@ if (true == perform_query( $dbc, $query ) ) { //run the query
   	$count=mysqli_num_rows($result); //count the rows returned
 		if($count != 0) { //if count is not 0,
          		random_password(); //run function random_password
-         		$encryptnewpassword = sha1($newpassword); //encrypt new password from function random_password()
-         		$query = "UPDATE Member SET password='$encryptnewpassword' WHERE email='$email'"; //query to replace password in database
-		
-			$dbc = connect_to_db("kernanc"); //connect to database
-			if (true == perform_query( $dbc, $query ) ) { //run the query
-				$returnstatus = array( 'status'=> "success", 'data'=> "You have successfully changed your password");
-				echo $returnstatus;
-			} else {
-				$returnstatus = array( 'status'=> "failure", 'data'=> "Error: " . $sql . "<br>" . mysqli_error($connect));
-				echo $returnstatus;
-			}
 		} else { //if count is 0, return error message
 			$error = "Your email is invalid";
-			echo $error;
+			echo $error;	
 		}
 }
 
-// Generate a random password and email to user
+// Generate a random password
 function random_password() {
 	$chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
 	$newpassword = array();
@@ -38,12 +27,24 @@ function random_password() {
 		$n = rand(0, $charsLength);
 		$newpassword[] = $chars[$n];
 	}
-
+//email new password to user
 	$to = isset($_POST["email"]) ? $_POST["email"] : "";
 	$subject = "Forgot Your Password";
 	$body = "Here is your new password: $newpassword";
 	$header = "From: The-Social-Network@bc.edu";
-		mail($to, $subject, $body, $headers);
+		mail($to, $subject, $body, $header);
+//encrypt new password and update database		
+	$encryptnewpassword = sha1($newpassword); //encrypt new password
+        $query = "UPDATE Member SET password='$encryptnewpassword' WHERE email='$email'"; //query to replace password in database
+		
+	$dbc = connect_to_db("kernanc"); //connect to database
+		if (true == perform_query( $dbc, $query ) ) { //run the query
+			$returnstatus = array( 'status'=> "success", 'data'=> "You have successfully changed your password");
+			echo $returnstatus;
+		} else {
+			$returnstatus = array( 'status'=> "failure", 'data'=> "Error: " . $sql . "<br>" . mysqli_error($connect));
+			echo $returnstatus;
+		}
 }
 
 ?>
