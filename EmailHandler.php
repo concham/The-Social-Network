c75263731d3c02cd9f5b5af47b3c4d644ae3b0f9
cmkernan <kernanc@bc.edu>
2016-05-02 09:57:33 -0400

    Update EmailHandler.php

--- EmailHandler.php
+++ EmailHandler.php
@@ -2,7 +2,34 @@
 
 include("dbconn.php");
 
-$school = array(); //sets the schools as an array
+if(!empty($_POST['school[]'])) {
+	foreach($_POST['school[]'] as $schools){
+	}
+}
+
+$splitarray = join("OR", $schools);
+$query = "SELECT email FROM Member WHERE school='$splitarray'";
+
+$dbc = connect_to_db("kernanc");
+if (true == perform_query( $dbc, $query ) ) {
+	$result = mysqli_query($dbc, $query);
+	while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+		$email = $row['email'];
+		$emaillist = "";
+		$emaillist += "$email, ";
+	}
+}
+
+$to = $emaillist;
+$subject = isset($_POST["subject"]) ? $_POST["subject"] : "";
+$message = isset($_POST["message"]) ? $_POST["message"] : "";
+$header = 'From: The-Social-Network@bc.edu';
+	mail( $to, $subject, $message, $header );
+	
+	
+?>
+	
+/* $school = array(); //sets the schools as an array
 $school[] = (isset($_POST['as']) && $_POST['as'] == 1) ? 'school_name = Morrissey College of Arts & Sciences ' : '';
 $school[] = (isset($_POST['law']) && $_POST['law'] == 1) ? 'school_name = Law School' : '';
 $school[] = (isset($_POST['woods']) && $_POST['woods'] == 1) ? 'school_name = Woods College of Advancing Studies ' : '';
@@ -21,20 +48,4 @@ if (!empty($temparray)) {
 
 $query = "SELECT email FROM Member $selection"; // selects from the database based on which criteria is set - some, all or none
 
-$dbc = connect_to_db("kernanc");
-if (true == perform_query( $dbc, $query ) ) {
-	$result = mysqli_query($dbc, $query);
-	while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-		$email = $row['email'];
-		$emaillist = "";
-		$emaillist += "$email, ";
-	}
-}
-
-$to = $emaillist;
-$subject = isset($_POST["subject"]) ? $_POST["subject"] : "";
-$message = isset($_POST["message"]) ? $_POST["message"] : "";
-$header = 'From: The-Social-Network@bc.edu';
-	mail( $to, $subject, $message, $header );
-
-?>
+?> */
