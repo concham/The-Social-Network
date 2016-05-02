44cd128421d56516e219ee3e0dabd56e09ea845e
cmkernan <kernanc@bc.edu>
2016-05-02 10:09:31 -0400

    Update EmailHandler.php

--- EmailHandler.php
+++ EmailHandler.php
@@ -26,22 +26,3 @@ if(!empty($_POST['school[]'])) {
 }
 
 ?>
-	
-/* $school = array(); //sets the schools as an array
-$school[] = (isset($_POST['as']) && $_POST['as'] == 1) ? 'school_name = Morrissey College of Arts & Sciences ' : '';
-$school[] = (isset($_POST['law']) && $_POST['law'] == 1) ? 'school_name = Law School' : '';
-$school[] = (isset($_POST['woods']) && $_POST['woods'] == 1) ? 'school_name = Woods College of Advancing Studies ' : '';
-$school[] = (isset($_POST['social']) && $_POST['social'] == 1) ? 'school_name = School of Social Work ' : '';
-$school[] = (isset($_POST['csom']) && $_POST['csom'] == 1) ? 'school_name = Carroll School of Management ' : '';
-$school[] = (isset($_POST['nursing']) && $_POST['nursing'] == 1) ? 'school_name = Connell School of Nursing ' : '';
-$school[] = (isset($_POST['lynch']) && $_POST['lynch'] == 1) ? 'school_name = Lynch School of Education ' : '';
-$school[] = (isset($_POST['theology']) && $_POST['theology'] == 1) ? 'school_name = School of Theology & Ministry ' : '';
-
-$temparray = array_filter($school); //filters the array for empty values
-if (!empty($temparray)) {
-   $selection = "WHERE school='.join('OR',$temparray)";   // assigns a WHERE-clause with the non-empty values in the $temparray
-} else {
-   $selection = ''; 
-}
-
-$query = "SELECT email FROM Member $selection"; // selects from the database based on which criteria is set - some, all or none */
