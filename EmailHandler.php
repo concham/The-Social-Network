7bb2b7f8e2b935d7d046df223ea6422304a6129d
cmkernan <kernanc@bc.edu>
2016-05-03 17:05:26 -0400

    Update EmailHandler.php

--- EmailHandler.php
+++ EmailHandler.php
@@ -2,28 +2,33 @@
 
 include("dbconn.php");
 
-$query = "SELECT email FROM Member WHERE";
-if(!empty($_POST['school'])) {
-	foreach($_POST['school'] as $selected) {
-		$splitarray = join("OR", $selected);
-		$query += "school='$splitarray'";
+$schools = explode( ",", $_POST["schools"] );
+//print_r($_POST);
+//print_r($schools);
 
-		$dbc = connect_to_db("kernanc");
-		if (true == perform_query( $dbc, $query ) ) {
-			$result = mysqli_query($dbc, $query);
-			while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-				$email = $row['email'];
-				$emaillist = "";
-				$emaillist += "$email, ";
-				
-					$to = $emaillist;
-					$subject = isset($_POST["subject"]) ? $_POST["subject"] : "";
-					$message = isset($_POST["message"]) ? $_POST["message"] : "";
-					$header = 'From: The-Social-Network@bc.edu';
-						mail( $to, $subject, $message, $header );
-			}
-		}
+$wherestr = "";
+foreach ($schools as $val) {
+	if ($wherestr == "") {
+		$wherestr = "school=$val";
+	} else {
+		$wherestr  .= " OR school=$val";
 	}
 }
+
+$query = "SELECT email FROM Member WHERE $wherestr";
+		$emaillist = "";
+		$dbc = connect_to_db("kernanc");
+		$result = perform_query( $dbc, $query );
+ 
+		while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+			$email = $row['email'];
+			$emaillist .= "$email, ";
+		}
+		
+		$to = $emaillist;
+		$subject = isset($_POST["subject"]) ? $_POST["subject"] : "";
+		$message = isset($_POST["message"]) ? $_POST["message"] : "";
+		$header = 'From: The-Social-Network@bc.edu';
+				mail( $to, $subject, $message, $header );
 
 ?>
